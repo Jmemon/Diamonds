fc069b5fabfe69caac2a39bb0f832aa5d1b4ad45
jmemon <johnmemon@gmail.com>
2022-03-28 21:10:08 -0400

    Model

--- Model.py
+++ Model.py
@@ -1,25 +1,42 @@
+import torch
 from torch import nn
 
 
 class PricePredictor(nn.Module):
 
+    channels = [3, 16, 32]
+
     def __init__(self):
         super(PricePredictor, self).__init__()
-        in_layer = nn.Conv2d(in_channels=(3, 400, 400),
-                             out_channels=(8, 200, 200),
-                             kernel_size=(5, 5),
-                             stride=2)  # layer dimensions organized as (C, H, W)
-        h1 = nn.Conv2d(in_channels=(8, 200, 200),
-                       out_channels=(8, 100, 100),
-                       kernel_size=(5, 5),
-                       stride=2)
-        h2 = nn.Conv2d(in_channels=(8, 100, 100),
-                       out_channels=(16, 50, 50),
-                       kernel_size=(3, 3))
-        flat = nn.Flatten()
-        linear1 = nn.Linear(40000, 10000)
-        linear2 = nn.Linear(10000, 1000)
-        out_layer = nn.Linear(1000, 1)
+        self.conv_layers = nn.Sequential(
+            *[self.conv_block(idx) for idx in range(len(self.channels))]
+        )
+        self.flatten = nn.Flatten()
+        self.linear_layers = nn.Sequential(
+            nn.Linear(28_227, 1000),
+            nn.ReLU(inplace=False),
+            nn.Linear(1000, 1),
+            nn.ReLU(inplace=False)
+        )
+        self.prediction_layer = nn.Sequential(  # input the weight value here
+            nn.Linear(2, 1),
+            nn.ReLU(inplace=False)
+        )
 
-    def forward(self, x):
-        pass
+    # Pass 1: 400x400x3  -> 396x396x16 -> 198x198x16
+    # Pass 2: 198x198x16 -> 194x194x32 -> 97x97x32
+    def conv_block(self, in_chan_idx: int) -> nn.Module:
+        return nn.Sequential(
+            nn.Conv2d(in_channels=self.channels[in_chan_idx],
+                      out_channels=self.channels[in_chan_idx + 1],
+                      kernel_size=(5, 5),
+                      stride=(1, 1)),
+            nn.MaxPool2d(2, 2),
+            nn.ReLU(inplace=False)
+        )
+
+    def forward(self, x_img: torch.Tensor, x_weight: torch.Tensor) -> torch.Tensor:
+        x_img = self.conv_layers(x_img)
+        x_img = self.flatten(x_img)
+        x_img = self.linear_layers(x_img)
+        return self.prediction_layer(torch.tensor([x_img.item(), x_weight.item()]))
